5f827cd1946f33f1d3a3f2dc736f3111e8040c3e
Kamran <105550218+kamransadiqali@users.noreply.github.com>
2025-09-19 12:31:23 +0500

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Demo PHP Page</title>
+    <style>
+        body {
+            background-color: green;
+            color: white; /* Adding white text color for better readability */
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Hello, world!\n";
+    echo "version 2";
+    ?>
+</body>
+</html>
